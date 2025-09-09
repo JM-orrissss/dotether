7a729d737855952f6e9185223a5310cd3b2487d4
James Morris <james5432morris@gmail.com>
2025-09-09 18:32:02 +0100

    Commented unused code

--- src/app/currencies/page.tsx
+++ src/app/currencies/page.tsx
@@ -1,6 +1,3 @@
-import { contentfulQuery } from "@/lib/contentfulClient";
-import { componentHeader } from "@/lib/queries/componentQueries";
-import React from "react";
 
 export default async function CurrenciesPage() {
     // const data = await contentfulQuery(componentHeader)
