e7e4ee489132114c4ac43da4623e10522fb1a216
James Morris <james5432morris@gmail.com>
2025-09-09 18:31:17 +0100

    Commented unused code

--- src/app/currencies/page.tsx
+++ src/app/currencies/page.tsx
@@ -3,8 +3,8 @@ import { componentHeader } from "@/lib/queries/componentQueries";
 import React from "react";
 
 export default async function CurrenciesPage() {
-    const data = await contentfulQuery(componentHeader)
-    const entries = data.componentHeaderCollection.items;
+    // const data = await contentfulQuery(componentHeader)
+    // const entries = data.componentHeaderCollection.items;
 
     return (
         <div>Sample currencies page</div>
